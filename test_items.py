f8f16199606410b1fdf44c7498f10957119b19aa
Agitik <62553989+Agitik@users.noreply.github.com>
2021-07-22 00:46:15 +0300

    Добавил assert

--- test_items.py
+++ test_items.py
@@ -1,8 +1,7 @@
-import time
-
 link = 'https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
 
 def test_user_should_see_addtshoplist_btn(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#add_to_basket_form > button")
-    time.sleep(30)
+    count = browser.find_elements_by_css_selector("#add_to_basket_form > button")
+    assert len(count) == 1
